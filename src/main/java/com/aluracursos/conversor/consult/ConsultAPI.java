703224e0c247ab295fd96ef432bb86bceaefa2c0
GILBERTOTABARES <gilbertotabares2010@gmail.com>
2024-10-07 12:34:35 -0500

    proyecto subido

---
+++ src/main/java/com/aluracursos/conversor/consult/ConsultAPI.java
@@ -0,0 +1,31 @@
+package com.aluracursos.conversor.consult;
+
+import com.aluracursos.conversor.model.Money;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultAPI {
+    public Money convert(String baseCurrency, String targetCurrency, Double amount){
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/09f2a1ae9c92c517be25fb21/pair/"
+                + baseCurrency + "/" + targetCurrency + "/" + amount);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Money.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+//diseñado por Gilberto Tabares
